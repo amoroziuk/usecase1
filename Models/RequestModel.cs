1664960963e8b9c7ea1466cef97bd06c70a24a17
Andrii Moroziuk <amoro@softserveinc.com>
2023-08-25 21:29:49 +0300

    UC#1 Create sort function by name

--- Models/RequestModel.cs
+++ Models/RequestModel.cs
@@ -4,4 +4,5 @@ public class RequestModel
 {
     public string Name { get; set; }
     public long? Population { get; set; }
+    public string Sort { get; set; }
 }
--- Services/CountryService.cs
+++ Services/CountryService.cs
@@ -28,14 +28,25 @@ public class CountryService : ICountryService
     public async Task<IEnumerable<Country>> GetFilteredCountriesAsync(RequestModel model)
     {
         var allCountries = await GetAllCountriesAsync();
+        var result = new List<Country>();
 
         if (!string.IsNullOrWhiteSpace(model.Name))
-            return allCountries?.Where(x => x.Name.Common.ToLower().Contains(model.Name.ToLower()));
+            allCountries = allCountries?.Where(x => x.Name.Common.ToLower().Contains(model.Name.ToLower()));
 
         if (model.Population.HasValue)
         {
             var populationExact = model.Population.Value * 1000000; // Convert input from millions to the exact number
-            return allCountries?.Where(x => x.Population < populationExact);
+            allCountries = allCountries?.Where(x => x.Population < populationExact);
+        }
+
+        if (!string.IsNullOrWhiteSpace(model.Sort))
+        {
+            allCountries = model.Sort?.ToLower() switch
+            {
+                "ascend" => allCountries.OrderBy(item => item.Name.Common).ToList(),
+                "descend" => allCountries.OrderByDescending(item => item.Name.Common).ToList(),
+                _ => allCountries // If sort parameter is not provided or invalid, return items as is.
+            };
         }
 
         return allCountries;
